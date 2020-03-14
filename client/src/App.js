602c287696dc036e5880187db6c2bdae36c93a1a
cecil john tantay <cecil.tantay@latimes.com>
2020-03-14 14:15:12 -0700

    create 8x8 arr and set to state

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    // create 8x8 grid
+    const grid = new Array(8).fill(new Array(8).fill(0));
+    // initialize with checkers in position
+    const init = grid.map((outer, i) => {
+      if (i === 5 || i === 7) {
+        return outer.map((inner, index) => {
+          if (index === 1 || index === 3 || index === 5 || index === 7) {
+            return (inner = 1);
+          }
+          return inner;
+        });
+      } else if (i === 6) {
+        return outer.map((inner, index) => {
+          if (index === 0 || index === 2 || index === 4 || index === 6) {
+            return (inner = 1);
+          }
+          return inner;
+        });
+      }
+      return outer;
+    });
+
+    setState(init);
+  }, []);
+
+  if (!state.length) return <h1>Loading...</h1>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
     </div>
   );
 }
